be2defb223bbaa7421480a9bfc06d06fd088cfbf
Ville Oinonen <muussari@hotmail.com>
2019-04-05 12:15:37 +0300

    merge fix

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<?php 
+    require "head.php";
+    
+?>
+<div class="container-fluid">
+<header>
+<?php 
+require "header.php";
+?></header>
+<div class="card"> 
+            <div class="card-body">
+            <h1>Teacher Dashboard</h1>
+            </div> 
+        </div>
+
+<div id="dashCont">
+  
+    <div id="dashLeft">
+        <div class="card">
+            <div class="card-header"><h2>Select student: </h2></div>
+            <div class="card-body">
+                <ul>
+                    <li><h3>Student 1</h3></li>
+                    <li><h3>Student 2</h3></li>
+                    <li><h3>Student 3</h3></li>
+                    <li><h3>Student 4</h3></li>
+                    <li><h3>Student 5</h3></li>
+                </ul>
+            </div> 
+        </div>
+        </div>
+
+    <div id="dashMid">
+    <div class="card">
+            <div class="card-header"><h2>Quizzes completed and points: </h2></div>
+            <div class="card-body">
+                <ul>
+                    <li><h3>Student 1</h3></li>
+                    <li><h3>Student 2</h3></li>
+                    <li><h3>Student 3</h3></li>
+                    <li><h3>Student 4</h3></li>
+                    <li><h3>Student 5</h3></li>
+                </ul>
+            </div> 
+        </div>    
+    <</div>
+    <div id="dashright">
+    <div class="card">
+            <div class="card-header"><h2>Something?</h2></div>
+            <div class="card-body">
+                <ul>
+                    <li><h3>Student 1</h3></li>
+                    <li><h3>Student 2</h3></li>
+                    <li><h3>Student 3</h3></li>
+                    <li><h3>Student 4</h3></li>
+                    <li><h3>Student 5</h3></li>
+                </ul>
+            </div> 
+        </div> 
+    </div>
+    
+</div>
+</div>

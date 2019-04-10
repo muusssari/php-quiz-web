cf2f8908861c7d71adc12130919e9262347df40c
Ville Oinonen <muussari@hotmail.com>
2019-04-10 13:25:13 +0300

    for show

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,8 @@
 <?php 
     require "head.php";
-    
+    require 'includes/dbh.inc.php';
 ?>
 <div class="container-fluid">
-<header>
-<?php 
-require "header.php";
-?></header>
 <div class="card"> 
             <div class="card-body">
             <h1>Teacher Dashboard</h1>
@@ -20,11 +16,15 @@ require "header.php";
             <div class="card-header"><h2>Select student: </h2></div>
             <div class="card-body">
                 <ul>
+                    <?php
+                    $students = $conn->query("SELECT * FROM users");
+                    while($row = $students->fetch_assoc()) {
+                        if($row["permiss"] == false) {
+                            echo '<li><button type="button" onclick="studentScores('.$row["idUsers"].')"><h3>'.$row["uidUsers"].'</h3></button></li>';
+                        }
+                    }
+                    ?>
                     <li><h3>Student 1</h3></li>
-                    <li><h3>Student 2</h3></li>
-                    <li><h3>Student 3</h3></li>
-                    <li><h3>Student 4</h3></li>
-                    <li><h3>Student 5</h3></li>
                 </ul>
             </div> 
         </div>
@@ -43,20 +43,6 @@ require "header.php";
                 </ul>
             </div> 
         </div>    
-    <</div>
-    <div id="dashright">
-    <div class="card">
-            <div class="card-header"><h2>Something?</h2></div>
-            <div class="card-body">
-                <ul>
-                    <li><h3>Student 1</h3></li>
-                    <li><h3>Student 2</h3></li>
-                    <li><h3>Student 3</h3></li>
-                    <li><h3>Student 4</h3></li>
-                    <li><h3>Student 5</h3></li>
-                </ul>
-            </div> 
-        </div> 
     </div>
     
 </div>

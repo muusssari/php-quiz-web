50fa700a24b99adc4c31a2df37322ea2fc2273f8
federaljules <julius.kerminen@gmail.com>
2019-04-10 11:08:26 +0300

    create.php styles

--- createquiz/main.js
+++ createquiz/main.js
@@ -3,7 +3,7 @@ let start;
 let layer1ID = 0;
 let layer2ID = 0;
 
-
+generateCreate();
 function generateCreate() {
     document.getElementById('id').innerHTML='';
     var f = document.createElement("form");
@@ -12,30 +12,42 @@ function generateCreate() {
         i.setAttribute('type',"text");
         i.setAttribute('id',"question");
         i.setAttribute('placeholder','question');
+        i.setAttribute('class', 'form-control');
+
 
         var i2 = document.createElement("input");
         i2.setAttribute('type',"text");
         i2.setAttribute('id',"rightAns");
         i2.setAttribute('placeholder','rightAns');
+        i2.setAttribute('class', 'form-control');
 
         var i3 = document.createElement("input");
         i3.setAttribute('type',"text");
         i3.setAttribute('id',"rightAnsQ");
         i3.setAttribute('placeholder','rightAnsQ');
+        i3.setAttribute('class', 'form-control');
+
 
         var i4 = document.createElement("input");
         i4.setAttribute('type',"text");
         i4.setAttribute('id',"qType");
         i4.setAttribute('placeholder','TRUE OR FALSE');
+        i4.setAttribute('class', 'form-control');
+
 
         var s = document.createElement("input");
         s.setAttribute('type',"button");
         s.setAttribute('value',"Submit");
         s.setAttribute('onclick','save3()');
+        s.setAttribute('class', 'btn btn-success create_btn');
+
+
         var s2 = document.createElement("input");
         s2.setAttribute('type',"button");
         s2.setAttribute('value',"NextSub");
         s2.setAttribute('onclick','save3Back2()');
+        s2.setAttribute('class', 'btn btn-outline-light create_btn');
+
 
         f.appendChild(i2);
         f.appendChild(i3);
@@ -45,35 +57,49 @@ function generateCreate() {
         i.setAttribute('type',"text");
         i.setAttribute('id',"sub2");
         i.setAttribute('placeholder','subname2');
+        i.setAttribute('class', 'form-control');
+
 
         var s = document.createElement("input");
         s.setAttribute('type',"button");
         s.setAttribute('value',"Submit");
         s.setAttribute('onclick','save2()');
+        s.setAttribute('class', 'btn btn-success create_btn');
+
         var s2 = document.createElement("input");
         s2.setAttribute('type',"button");
         s2.setAttribute('value',"NextQuiz");
         s2.setAttribute('onclick','save2Back1()');
+        s2.setAttribute('class', 'btn btn-outline-light create_btn');
+
     }else {
 
         var i = document.createElement("input");
         i.setAttribute('type',"text");
         i.setAttribute('id',"quiz");
         i.setAttribute('placeholder','quizname');
+        i.setAttribute('class', 'form-control');
+
 
         var i2 = document.createElement("input");
         i2.setAttribute('type',"text");
         i2.setAttribute('id',"subname");
         i2.setAttribute('placeholder','subname');
+        i2.setAttribute('class', 'form-control');
+
 
         var s = document.createElement("input");
         s.setAttribute('type',"button");
         s.setAttribute('value',"Submit");
         s.setAttribute('onclick','save1()');
+        s.setAttribute('class', 'btn btn-success create_btn');
+
         var s2 = document.createElement("input");
         s2.setAttribute('type',"button");
         s2.setAttribute('value',"Done");
         s2.setAttribute('onclick','done()');
+        s2.setAttribute('class', 'btn btn-outline-light create_btn');
+
 
         f.appendChild(i2);
     }
@@ -98,7 +124,7 @@ function save1() {
     generateCreate();
 }
 function done() {
-    let done = $("<i></i>").text("Done!");
+    let done = $("<i class='doneStyle'></i>").text("Done!");
     $('#id').append(done);
 }
 

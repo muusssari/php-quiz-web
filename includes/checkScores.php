3e6e0bc722980ad106718ea7cf16e2dd5542c234
Ville Oinonen <muussari@hotmail.com>
2019-04-11 10:45:24 +0300

    dashboard scoreScreen

--- includes/checkScores.php
+++ includes/checkScores.php
@@ -9,18 +9,29 @@ if(isset($_POST['id'])) {
     //$number=mysqli_num_rows($quiz);
     for($x = 0; $x < 20; $x++) {
         while($row = $quiz->fetch_assoc()) {
-            echo "<h3>Quiz Name: " . $row['quiz']. "</h3><br>";
+            echo "<h3>Quiz Name: " . $row['quiz'];
             $scores = 0;
+            $done = false;
             $qID = $row['idQuiz'];
             $collect = $conn->query("SELECT * FROM collect WHERE idQuiz=$qID");
             while($row = $collect->fetch_assoc()) {
                 $idQ = $row['idQuestion'];
-                $ans = $conn->query("SELECT points FROM userAns WHERE idQuestion=$idQ AND idUsers=$id");
+                $ans = $conn->query("SELECT * FROM userAns WHERE idQuestion=$idQ AND idUsers=$id");
                 while($row = $ans->fetch_assoc()) {
                     $scores += $row['points'];
-
+                    if($row['submit'] == 1) {
+                        $done = true;
+                    }else {
+                        $done = false;
+                    }
                 }
             }
+            if($done == 1){
+                echo " Done </h3><br>";
+            }else {
+                echo "</h3><br>";
+            }
+            
             echo "<h5>Scores: " . $scores . "</h5><br>";
         }
     }

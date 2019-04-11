40131732996aa5ff2af8003afa56d263a52ce2df
Ville Oinonen <muussari@hotmail.com>
2019-04-11 11:57:05 +0300

    select quiz fixed

--- quiz_select.php
+++ quiz_select.php
@@ -10,9 +10,34 @@ if($_SESSION['permiss'] == 1) {
     $quizName = $conn->query("SELECT * FROM quiz");
     $user = $_SESSION['userId'];
     echo "<h1 class='selectH'>Select your quiz</h1><br>";
+
     while($row = $quizName->fetch_assoc()) {
         $idQ = $row["idQuiz"];
-        echo "<li><a href='index.php?page=".$row["idQuiz"]."'><button style='font-size:25pt;' class='btn btn-outline-dark btn-block'>" . $row["quiz"]. "</button></a></li>";
+        $qName = $row["quiz"];
+        $done = false;
+        $collect = $conn->query("SELECT * FROM collect WHERE idQuiz=$idQ");
+        while($row = $collect->fetch_assoc()) {
+
+            $coll = $row["idQuestion"];
+            $ans = $conn->query("SELECT * FROM userAns WHERE idUsers=$user AND idQuestion=$coll");
+                while($row = $ans->fetch_assoc()) {
+                    $s = $row['submit'];
+                    echo "<script>console.log( 'Debug Objects 2: " . $s . "' );</script>";
+                    if($s== 1){
+                        $done = true;
+                    }else {
+                        $done = false;
+                    }
+                    
+                }
+            
+            
+        }
+        if($done) {
+            echo "<li><a><button style='font-size:25pt;' class='btn btn-outline-dark btn-block'>DONE</button></a></li>";
+        }else {
+            echo "<li><a href='index.php?page=".$idQ."'><button style='font-size:25pt;' class='btn btn-outline-dark btn-block'>" . $qName. "</button></a></li>";
+        }  
     }
     //Pitää korjata, jotta pisteet näkyisivät kun vastaukset on lähetetty, 
     //samalla estää quizin avaamisen uusiksi
